575a891b25f30f38d42022fe58090f3a0729e09c
Timo Müller <76396823+timoym@users.noreply.github.com>
2024-05-22 21:30:42 +0200

    implemented upload function

--- src/functions/upload.ts
+++ src/functions/upload.ts
@@ -1,18 +1,39 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
-const { BlobServiceClient } = require("@azure/storage-blob");
-const { v1: uuidv1 } = require("uuid");
+import { BlobServiceClient } from "@azure/storage-blob";
+import { Readable } from "stream";
+import { v1 as uuidv1 } from "uuid";
 require("dotenv").config();
 
-export async function upload(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+export async function uploadFunction(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    if (!request.body) {
+        return {
+            status: 400,
+            body: "Please pass a file in the request body"
+        };
+    }
 
-    const name = request.query.get('name') || await request.text() || 'world';
+    const fileId = uuidv1();
+    const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
+    const containerClient = blobServiceClient.getContainerClient("docx-files");
+    const blobClient = containerClient.getBlockBlobClient(fileId + ".docx");
 
-    return { body: `Hello, ${name}!` };
+    const uploadBlobResponse = await blobClient.uploadStream(Readable.from(request.body));
+
+    if (uploadBlobResponse._response.status !== 201) {
+        return {
+            status: 500,
+            body: `Failed to upload file. Status code: ${uploadBlobResponse._response.status}`
+        };
+    }
+
+    return {
+        status: 200,
+        body: `File uploaded successfully. File Id: ${fileId}`
+    };
 };
 
 app.http('upload', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
-    handler: upload
+    handler: uploadFunction
 });
